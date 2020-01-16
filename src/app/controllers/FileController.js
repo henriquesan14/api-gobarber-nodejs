98ecb092cd4bddaf20dbc7ddaaf5ada0eaa72c1d
jose henrique <jose.henrique@vsoft.com.br>
2020-01-16 15:42:44 -0300

    adicionado lib multer pra upload de arquivos e associate de usuario com avatar

---
+++ src/app/controllers/FileController.js
@@ -0,0 +1,9 @@
+class FileController {
+    async store(req, res){
+        return res.json(req.file);
+    }
+}
+
+export default new FileController();
+
+
--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import User from '../app/models/User';
+import File from '../app/models/File';
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
     constructor(){
@@ -11,7 +12,9 @@ class Database {
 
     init(){
         this.connetion = new Sequelize(databaseConfig);
-        models.map(model => model.init(this.connetion));
+        models
+        .map(model => model.init(this.connetion))
+        .map(model => model.associate && model.associate(this.connetion.models));
     }
 }
 
