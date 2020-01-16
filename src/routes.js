40c3f14b3abb15e7f827646c9a4c71d9d0991db8
jose henrique <jose.henrique@vsoft.com.br>
2020-01-16 10:45:20 -0300

    Controller de usuario

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/user';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'Henrique',
-        email: 'henrique@gmail.com',
-        password_hash: '1233456'
-    })
-    res.json(user);
-});
+routes.post('/users', UserController.store);
+
 export default routes;
