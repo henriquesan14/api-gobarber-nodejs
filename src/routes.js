445584bdae41f53966988f98515b561536cf8773
jose henrique <jose.henrique@vsoft.com.br>
2020-01-17 07:57:42 -0300

    Criado model de appointments e rota de cadastro

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index)
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file') , FileController.store)
 
 export default routes;
