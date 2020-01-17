8305da43fc764fd1e8a3a21a6b279d778ca17cef
jose henrique <jose.henrique@vsoft.com.br>
2020-01-17 11:03:19 -0300

    rota de listar agendamentos e de listar agendamentos por provider logado

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,8 +17,10 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
-routes.get('/providers', ProviderController.index)
+routes.get('/providers', ProviderController.index);
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file') , FileController.store)
 
 export default routes;
