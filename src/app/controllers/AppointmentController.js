bc3308b9e3dfbf24310a4e911c37587993e617a2
jose henrique <jose.henrique@vsoft.com.br>
2020-01-20 14:32:52 -0300

    configurando template de email com nodemailer, handlebars

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -96,6 +96,11 @@ class AppointmentController {
                     model: User,
                     as: 'provider',
                     attributes: ['name', 'email']
+                },
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name']
                 }
             ]
         });
@@ -111,7 +116,16 @@ class AppointmentController {
         await Mail.sendMail({
             to: `${appointment.provider.name} <${appointment.provider.email}>`,
             subject: 'Agendamento cancelado',
-            text: 'Você tem um novo cancelamento'
+            template: 'cancellation',
+            context: {
+                provider: appointment.provider.name,
+                user: appointment.user.name,
+                date: format(
+                    appointment.date,
+                    "'dia' dd 'de' MMMM', ás' H:mm'h'",
+                    {locale: pt}
+                )
+            }
         });
         return res.json(appointment);
     }
