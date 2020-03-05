80b022dfd730d086b5160b2ebdea0b2c7ea9ccd7
Henrique <jose.henrique@vsoft.com.br>
2020-03-05 15:24:04 -0300

    ajuste

--- src/app/services/AvailableService.js
+++ src/app/services/AvailableService.js
@@ -31,8 +31,6 @@ class AvailableService {
         const available = schedule.map(time => {
             const [hour, minute] = time.split(':');
             const value = setSeconds(setMinutes(setHours(date, hour), minute),0);
-            console.log(value);
-            console.log(format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"));
             return {
                 time,
                 value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
